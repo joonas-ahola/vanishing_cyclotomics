c20f306cd3c3de9c79778f260a2480c3817dd0bc
Joonas Ahola <37556039+joonas-ahola@users.noreply.github.com>
2024-07-29 20:36:16 +0300

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 use std::str::FromStr;
+use malachite::Integer;
 use rayon::iter::IntoParallelIterator;
 use rayon::iter::ParallelIterator;
-use malachite::Natural;
 use malachite::num::arithmetic::traits::Pow;
 use primal_sieve;
+use mathru::algebra::abstr::Polynomial;
 
 fn main() {
-    round_1(vec![2539,2543,2549,2551,2557,2579,2591,2593,2609,2617,2621,2633,2647,2657,2659,2663], 47055833460, 2);
-    round_1(vec![2671,2677,2683,2687,2689,2693,2699,2707,2711,2713,2719,2729,2731,2741,2749,2753], 47055833460, 2);
-    round_1(vec![2767,2777,2789,2791,2797,2801,2803,2819,2833,2837,2843,2851,2857,2861,2879,2887], 47055833460, 2);
-    round_1(vec![2897,2903,2909,2917,2927,2939,2953,2957,2963,2969,2971,2999,3001,3011,3019,3023], 47055833460, 2);
-    round_1(vec![3037,3041,3049,3061,3067,3079,3083,3089,3109,3119,3121,3137,3163,3167,3169,3181], 47055833460, 2);
-    round_1(vec![3187,3191,3203,3209,3217,3221,3229,3251,3253,3257,3259,3271,3299,3301,3307,3313], 47055833460, 2);
-    round_1(vec![3319,3323,3329,3331,3343,3347,3359,3361,3371,3373,3389,3391,3407,3413,3433,3449], 47055833460, 2);
-    round_1(vec![3457,3461,3463,3467,3469,3491,3499,3511,3517,3527,3529,3533,3539,3541,3547,3557], 47055833460, 2);
-    round_1(vec![3559,3571,3581,3583,3593,3607,3613,3617,3623,3631,3637,3643,3659,3671,3673,3677], 47055833460, 2);
+    round_1(vec![269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359], 47055833460, 8);
+    round_1(vec![367,373,379,383,389,397,401,409, 419,421,431,433,439,443,449,457], 47055833460, 8);
+    round_1(vec![461,463,467,479,487,491,499,503, 509,521,523,541,547,557,563,569], 47055833460, 8);
+    round_1(vec![571,577,587,593,599,601,607,613, 617,619,631,641,643,647,653,659], 47055833460, 8);
+    round_1(vec![661,673,677,683,691,701,709,719, 727,733,739,743,751,757,761,769], 47055833460, 8);
+    round_1(vec![773,787,797,809,811,821,823,827, 829,839,853,857,859,863,877,881], 47055833460, 8);
+    round_1(vec![883,887,907,911,919,929,937,941, 947,953,967,971,977,983,991,997], 47055833460, 8);
+    round_1(vec![1009,1013,1019,1021,1031,1033, 1039,1049,1051,1061,1063,1069,1087,1091,1093,1097], 47055833460, 8);
+    round_1(vec![1103,1109,1117,1123,1129,1151, 1153,1163,1171,1181,1187,1193,1201,1213,1217,1223], 47055833460, 8);
+    round_1(vec![1229,1231,1237,1249,1259,1277, 1279,1283,1289,1291,1297,1301,1303,1307,1319,1321], 47055833460, 4);
+    round_1(vec![1327,1361,1367,1373,1381,1399, 1409,1423,1427,1429,1433,1439,1447,1451,1453,1459], 47055833460, 4);
 }
 
-fn round_1(vec_of_primes: Vec<usize>, ub: usize, x: usize) -> () {
+fn round_1(vec_of_primes: Vec<usize>, ub: usize, x: isize) -> () {
 
     // vec_of_primes = a list (or vector) of primes being evaluated.
     // ub = upper bound for mod q.
@@ -26,12 +29,14 @@ fn round_1(vec_of_primes: Vec<usize>, ub: usize, x: usize) -> () {
         vec_of_primes
         .into_par_iter()
         .for_each(|n: usize| {
+            
+            /* */
 
             let mut s: isize = (n - 1) as isize;
-            let mut value_0: Natural = Natural::from(0 as usize);
+            let mut value_0: Integer = Integer::from(0 as usize);
 
             while s >= 0 {
-                let a: Natural = Natural::from(x).pow(s as u64);
+                let a: Integer = Integer::from(x).pow(s as u64);
                 value_0 += &a;
                 s -= 1;
             }
@@ -45,7 +50,7 @@ fn round_1(vec_of_primes: Vec<usize>, ub: usize, x: usize) -> () {
             .enumerate()
             .for_each(|prime: (usize, usize)| {
 
-            let value_1: Natural = Natural::from_str(&number_string)
+            let value_1: Integer = Integer::from_str(&number_string)
             .unwrap();
 
             if prime.0 == 0 {
@@ -55,11 +60,88 @@ fn round_1(vec_of_primes: Vec<usize>, ub: usize, x: usize) -> () {
             println!("Done 50% of the range for n = {}.", n);
             }
 
-            let divisor = Natural::from(prime.1);
-            if (value_1 % divisor) == Natural::from(0 as u32) {
+            let divisor = Integer::from(prime.1);
+            if (value_1 % divisor) == Integer::from(0 as u32) {
                 println!("Collision found for (p, q) = ({}, {}) at x = {}", prime.1, n, x);
             }
-           });
         });
+    });
 }
 
+fn round_2(vec_of_primes: Vec<usize>, ub: usize, x: isize) -> () {
+    // vec_of_primes = a list (or vector) of primes being evaluated.
+    // ub = upper bound for q, i.e. for the order of the field.
+    // x = cyclotomic polynomial evaluated at x.
+
+        vec_of_primes
+        .into_par_iter()
+        .for_each(|n: usize| {
+
+            // Evaluate the polynomial.
+
+            let p: usize = n;
+
+            let chebyshev_t_4: isize = Polynomial::from_chebyshev_t(4).eval((x/2) as f64) as isize;
+            let chebyshev_t_2: isize = Polynomial::from_chebyshev_t(2).eval((x/2) as f64) as isize;
+
+            let p_div_2: u32 = ((p-1)/2).try_into().unwrap();
+
+            let chebyshev_u_p_minus_1_pol: Polynomial<f64> = Polynomial::from_chebyshev_u(p_div_2 - 1);
+            let chebyshev_u_p_pol: Polynomial<f64> = Polynomial::from_chebyshev_u(p_div_2);
+
+            let final_pol: Polynomial<f64> = &chebyshev_u_p_minus_1_pol + &chebyshev_u_p_pol;
+
+            let mut value_0: Integer = Integer::from(0 as usize);
+            let mut value_1: Integer = Integer::from(0 as usize);
+
+            // First, evaluate the quotient.
+
+            let mut s_1: isize = final_pol.degree() as isize;
+            let mut s_2: isize = final_pol.degree() as isize;
+
+            let coeffs: Vec<f64> = final_pol.coef.clone();
+
+            while s_1 >= 0 {
+                let b: Integer = Integer::from(coeffs[s_1 as usize] as isize);
+                let a: Integer = Integer::from(chebyshev_t_4);
+                value_0 += &b * &a.pow(s_1 as u64);
+                s_1 -= 1;
+            }
+
+            // Second, evaluate the dividend.
+
+            while s_2 >= 0 {
+                let b: Integer = Integer::from(coeffs[s_2 as usize] as isize);
+                let a: Integer = Integer::from(chebyshev_t_2);
+                value_1 += &b * &a.pow(s_2 as u64);
+                s_2 -= 1;
+            }
+            
+            // Proceed with calculations.
+
+            let number_string: String = (&value_0 / &value_1).to_string();
+            let q: usize = (n - 1)*(n - 1);
+            let sieve = primal_sieve::Sieve::new(ub);
+
+            sieve.primes_from(1)
+            .skip_while(|f| *f < q)
+            .enumerate()
+            .for_each(|prime: (usize, usize)| {
+
+            let value_1: Integer = Integer::from_str(&number_string)
+            .unwrap();
+
+            if prime.0 == 0 {
+            println!("Calculations started for n = {}.", 4*n);
+            }
+            else if prime.0 % 1000000000  == 0 {
+            println!("Done 50% of the range for n = {}.", 4*n);
+            }
+
+            let divisor = Integer::from(prime.1);
+            if (value_1 % divisor) == Integer::from(0 as u32) {
+                println!("Collision found for (p, q) = ({}, {}) at x = {}", prime.1, 4*n, x);
+            }
+        });
+    });
+}
