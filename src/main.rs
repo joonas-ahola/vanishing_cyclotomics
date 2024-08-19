12facd5a42ae0b1040048a25bde526fc853a6ce7
Joonas Ahola <37556039+joonas-ahola@users.noreply.github.com>
2024-08-19 20:09:38 +0300

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,90 @@
-use std::time::Instant;
 use std::ops::{Add, Sub, Mul, Div};
 use std::str::FromStr;
 use malachite::Integer;
+use malachite::Natural;
 use rayon::iter::IntoParallelIterator;
 use rayon::iter::ParallelIterator;
 use malachite::num::arithmetic::traits::Pow;
+use malachite::num::arithmetic::traits::Lcm;
 use primal_sieve;
 
 #[derive(Clone)]
 pub struct Polynomial {
     pub coef: Vec<Integer>,
+}
+
+#[derive(Clone)]
+pub struct Fraction {
+    pub num: Integer,
+    pub denom: Natural,
+}
+
+impl Fraction {
+    fn reduce_to_integer(self) -> Integer {
+        self.num / Integer::from(self.denom)
+    }
+}
+
+impl Add for Fraction {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        if self.denom == 0 && rhs.denom == 0 {
+            return Fraction{ num: Integer::from(0 as usize), denom: Natural::from(0 as usize)}
+        }
+        else if self.denom == 0 {
+            return Fraction{ num: rhs.num, denom: rhs.denom}
+        }
+        else if rhs.denom == 0 {
+            return Fraction{ num: self.num, denom: self.denom}
+        }
+
+        let self_denom_clone: Natural = self.denom.clone();
+        let rhs_denom_clone: Natural = rhs.denom.clone();
+
+        let lcd: Natural = self_denom_clone.lcm(rhs_denom_clone);
+
+        let mul_1: Natural = &lcd / self.denom.clone();
+        let mul_2: Natural = &lcd / rhs.denom.clone();
+
+        Fraction{num: self.num * Integer::from(mul_1) + rhs.num * Integer::from(mul_2), denom: lcd}
+    }
+}
+
+impl Sub for Fraction {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        if self.denom == 0 && rhs.denom == 0 {
+            return Fraction{ num: Integer::from(0 as usize), denom: Natural::from(0 as usize)}
+        }
+        else if self.denom == 0 {
+            return Fraction{ num: rhs.num, denom: rhs.denom}
+        }
+        else if rhs.denom == 0 {
+            return Fraction{ num: self.num, denom: self.denom}
+        }
+
+        let self_denom_clone: Natural = self.denom.clone();
+        let rhs_denom_clone: Natural = rhs.denom.clone();
+
+        let lcd: Natural = self_denom_clone.lcm(rhs_denom_clone);
+
+        let mul_1: Natural = &lcd / self.denom.clone();
+        let mul_2: Natural = &lcd / rhs.denom.clone();
+
+        Fraction{num: self.num * Integer::from(mul_1) - rhs.num * Integer::from(mul_2), denom: lcd}
+    }
+
+}
+
+impl  Mul for Fraction {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        Fraction{num: self.num * rhs.num, denom: self.denom * rhs.denom}
+    }
+
 }
 
 impl Polynomial {
@@ -29,6 +104,7 @@ impl Polynomial {
             }
         }
     }
+
     fn chebyshev_u(n: usize) -> Polynomial {
         let _n: usize = 
         if n < 4 
@@ -53,6 +129,7 @@ impl Polynomial {
         table[n].clone()
 
     }
+
     fn eval(self, x: Integer) -> Integer {
         let mut s: isize = self.degree() as isize;
         let mut value: Integer = Integer::from(0 as usize);
@@ -63,9 +140,23 @@ impl Polynomial {
             value += b * _x.pow(s as u64);
             s -= 1;
         }
+        value
+    }
+
+    fn eval_frac(self, x: Fraction) -> Fraction {
+        let mut s: isize = self.degree() as isize;
+        let mut value: Fraction = Fraction {num: Integer::from(0 as usize), denom: Natural::from(0 as usize)};
+
+        while s >= 0 {
+            let _x: Fraction = x.clone();
+            let b: Integer = self.coef[s as usize].clone();
+            value = value + Fraction{ num: &b * _x.num.pow(s as u64) , denom: _x.denom.pow(s as u64)};
+            s -= 1;
+        }
 
         value
     }
+
     fn degree(&self) -> usize {
         self.coef.len() - 1
     }
@@ -157,21 +248,23 @@ impl Div for Polynomial {
         Polynomial { coef: quotient }
     }
 }
-
-
-
 fn main() {
-    round_2(vec![269,271,277,281,283,293,307,311, 313,317,331,337,347,349,353,359], 47055833460, 4);
-    round_2(vec![367,373,379,383,389,397,401,409, 419,421,431,433,439,443,449,457], 47055833460, 4);
-    round_2(vec![461,463,467,479,487,491,499,503, 509,521,523,541,547,557,563,569], 47055833460, 4);
-    round_2(vec![571,577,587,593,599,601,607,613, 617,619,631,641,643,647,653,659], 47055833460, 4);
-    round_2(vec![661,673,677,683,691,701,709,719, 727,733,739,743,751,757,761,769], 47055833460, 4);
-    round_2(vec![773,787,797,809,811,821,823,827, 829,839,853,857,859,863,877,881], 47055833460, 4);
-    round_2(vec![883,887,907,911,919,929,937,941, 947,953,967,971,977,983,991,997], 47055833460, 4);
-    round_2(vec![1009,1013,1019,1021,1031,1033, 1039,1049,1051,1061,1063,1069,1087,1091,1093,1097], 47055833460, 4);
-    round_2(vec![1103,1109,1117,1123,1129,1151, 1153,1163,1171,1181,1187,1193,1201,1213,1217,1223], 47055833460, 4);
-    round_2(vec![1229,1231,1237,1249,1259,1277, 1279,1283,1289,1291,1297,1301,1303,1307,1319,1321], 47055833460, 4);
-    round_2(vec![1327,1361,1367,1373,1381,1399, 1409,1423,1427,1429,1433,1439,1447,1451,1453,1459], 47055833460, 4);
+    round_1(vec![277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373], 47055833460, -3);
+    round_1(vec![379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463], 47055833460, -3);
+    round_1(vec![467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577], 47055833460, -3);
+    round_1(vec![587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673], 47055833460, -3);
+    round_1(vec![677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787], 47055833460, -3);
+    round_1(vec![797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887], 47055833460, -3);
+    round_1(vec![907,911,919,929,937,941,947,953,967,971,977,983,991,997,1009,1013], 47055833460, -3);
+    round_1(vec![1019,1021,1031,1033,1039,1049,1051,1061,1063,1069,1087,1091,1093,1097,1103,1109], 47055833460, -3);
+    round_1(vec![1117,1123,1129,1151,1153,1163,1171,1181,1187,1193,1201,1213,1217,1223,1229,1231], 47055833460, -3);
+    round_1(vec![1237,1249,1259,1277,1279,1283,1289,1291,1297,1301,1303,1307,1319,1321,1327,1361], 47055833460, -3);
+    round_1(vec![1367,1373,1381,1399,1409,1423,1427,1429,1433,1439,1447,1451,1453,1459,1471,1481], 47055833460, -3);
+    round_1(vec![1483,1487,1489,1493,1499,1511,1523,1531,1543,1549,1553,1559,1567,1571,1579,1583], 47055833460, -3);
+    round_1(vec![1597,1601,1607,1609,1613,1619,1621,1627,1637,1657,1663,1667,1669,1693,1697,1699], 47055833460, -3);
+    round_1(vec![1709,1721,1723,1733,1741,1747,1753,1759,1777,1783,1787,1789,1801,1811,1823,1831], 47055833460, -3);
+    round_1(vec![1847,1861,1867,1871,1873,1877,1879,1889,1901,1907,1913,1931,1933,1949,1951,1973], 47055833460, -3);
+    round_1(vec![1979,1987,1993,1997,1999,2003,2011,2017,2027,2029,2039,2053,2063,2069,2081], 47055833460, -3);
 }
 
 fn round_1(vec_of_primes: Vec<usize>, ub: usize, x: isize) -> () {
@@ -221,7 +314,6 @@ fn round_1(vec_of_primes: Vec<usize>, ub: usize, x: isize) -> () {
 }
 
 fn round_2(vec_of_primes: Vec<usize>, ub: usize, x: isize) -> () {
-    let now = Instant::now();
     
     // vec_of_primes = a list (or vector) of primes being evaluated.
     // ub = upper bound for mod q.
@@ -249,9 +341,6 @@ fn round_2(vec_of_primes: Vec<usize>, ub: usize, x: isize) -> () {
             let chebyshev_u_p_pol: Polynomial = Polynomial::chebyshev_u(p_div_2);
 
             let final_pol: Polynomial = chebyshev_u_p_minus_1_pol + chebyshev_u_p_pol;
-
-            let elapsed = now.elapsed();
-            println!("Elapsed: {:.2?}", elapsed);
 
             // Evaluate the divisor, and divided.
 
@@ -287,5 +376,61 @@ fn round_2(vec_of_primes: Vec<usize>, ub: usize, x: isize) -> () {
                 println!("Collision found for (p, q) = ({}, {}) at x = {}", prime.1, 4*n, x);
             }
         });
+    });
+}
+
+fn round_3(vec_of_primes: Vec<usize>, ub: usize, x: i32) {
+
+    vec_of_primes
+        .into_par_iter()
+        .for_each(|n: usize| {
+
+        let x_fraction: Fraction = Fraction { num: Integer::from(x.clone()), denom: Natural::from(2 as usize) };
+        let x_fraction_1: Fraction = Fraction { num: Integer::from(x.clone()), denom: Natural::from(2 as usize) };
+    
+        let chebyshev_t_2_pol: Polynomial = Polynomial::chebyshev_t(2);
+        let chebyshev_t_4_pol: Polynomial = Polynomial::chebyshev_t(4);
+
+        let chebyshev_t_2: Fraction = chebyshev_t_2_pol.eval_frac(x_fraction);
+        let chebyshev_t_4: Fraction = chebyshev_t_4_pol.eval_frac(x_fraction_1);
+
+        let p: usize = n;
+        let p_div_2: usize = ((p-1)/2).try_into().unwrap();
+        
+        let chebyshev_u_p_minus_1_pol: Polynomial = Polynomial::chebyshev_u(p_div_2 - 1);
+        let chebyshev_u_p_pol: Polynomial = Polynomial::chebyshev_u(p_div_2);
+
+        let final_pol: Polynomial = chebyshev_u_p_minus_1_pol + chebyshev_u_p_pol;
+        let final_pol_clone: Polynomial = Polynomial{ coef: final_pol.coef.clone() };
+
+        let value_0: Fraction = final_pol.eval_frac(chebyshev_t_4);
+        let value_1: Fraction = final_pol_clone.eval_frac(chebyshev_t_2);
+
+        let value_0_int = value_0.reduce_to_integer();
+        let value_1_int = value_1.reduce_to_integer();
+        let number_string: String = (value_0_int / value_1_int).to_string();
+
+        let q: usize = (n - 1)*(n - 1);
+        let sieve = primal_sieve::Sieve::new(ub);
+
+        sieve.primes_from(1)
+        .skip_while(|f| *f < q)
+        .enumerate()
+        .for_each(|prime: (usize, usize)| {
+
+        let value_1: Integer = Integer::from_str(&number_string).unwrap();
+
+        if prime.0 == 0 {
+            println!("Calculations started for n = {}.", 4*n);
+        }
+        else if prime.0 % 1000000000  == 0 {
+            println!("Done 50% of the range for n = {}.", 4*n);
+        }
+
+        let divisor = Integer::from(prime.1);
+        if (value_1 % divisor) == Integer::from(0 as u32) {
+            println!("Collision found for (p, q) = ({}, {}) at x = {}", prime.1, 4*n, x);
+        }
+      });
     });
 }
